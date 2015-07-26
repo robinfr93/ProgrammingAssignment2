ac39f351c9a55cf4e49cec7d2c6b921390dc1a11
robinfr93 <robinfr93@gmail.com>
2015-07-26 20:12:51 +0530

    Edited by:Robin Francis, Course:R programming

--- cachematrix.R
+++ cachematrix.R
@@ -1,25 +1,28 @@
-
+## -----------------------------------------------------------------------------
 ## The following is a pair of functions that cache and compute the 
-## inverseMatrix of a matrix.
-## This function creates a special "matrix" object
-## that can cache its inverseMatrix.
+## inverse of a matrix.
+## This function creates a special "myMatrix" object
+## that can cache its inverse.
+## -----------------------------------------------------------------------------
 
 makeCacheMatrix = function(myMatrix = matrix()) {
-  inverseMatrix = NULL
-  set = function(x) {
+  inverseMatrix = NULL   
+  set = function(x) {    
     myMatrix <<- x;
     inverseMatrix <<- NULL;
   }
-  get = function() return(myMatrix);
-  setinv = function(inv) inverseMatrix <<- inv;
-  getinv = function() return(inverseMatrix);
+  get = function() return(myMatrix);    
+  setinv = function(inv) inverseMatrix <<- inv; 
+  getinv = function() return(inverseMatrix); 
   return(list(set = set, get = get, setinv = setinv, getinv = getinv))
 }
-## This function computes the inverseMatrix of the special
-## "matrix" returned by `makeCacheMatrix` above. If the inverseMatrix has
+
+## -----------------------------------------------------------------------------
+## This function computes the inverse of the special
+## "myMatrix" returned by `makeCacheMatrix` above. If the inverseMatrix has
 ## already been calculated (and the matrix has not changed), then
 ## `cacheSolve` should retrieve the inverseMatrix from the cache.
-
+## -----------------------------------------------------------------------------
 cacheSolve = function(myMatrix, ...) {
   inverseMatrix = myMatrix$getinv()
   if(!is.null(inverseMatrix)) {
